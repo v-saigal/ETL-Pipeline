80e931b3f6c31678648445277b07f3a1f80fbca2
Vishal <vishalsaigal27@gmail.com>
2021-08-17 10:17:50 +0100

    Updated extract function to return list of dicts

--- src/ETL/extract.py
+++ src/ETL/extract.py
@@ -1,15 +1,22 @@
 import csv
 import json
+from os import remove
 
 pathway = "../../data/2021-02-23-isle-of-wight.csv"
-fieldnames = ['']
+fieldnames = ["timestamp","store_name","customer_name","basket", "payment_type", "total_price","card_number"]
+keys_to_remove = ["customer_name", "card_number"]
+
 
 def extract(pathway):
     with open(pathway, mode='r') as csv_file:
-        reader = csv.DictReader(csv_file)
+        reader = csv.DictReader(csv_file, fieldnames=fieldnames)
         
-        for row in reader:
-            print(json.dumps(row, indent=4, sort_keys=True))
+        
+        # for row in reader:
+        #     print(json.dumps(row, indent=4, sort_keys=True))
+        
+        return list(reader)
 
 
-extract(pathway)
+
+
