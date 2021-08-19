48e468f69fb964b3e45483bb5413072e68edf9c9
Vishal <vishalsaigal27@gmail.com>
2021-08-19 15:50:14 +0100

    functions to change product to lower case and remove sensitive data

--- src/ETL/transform.py
+++ src/ETL/transform.py
@@ -1,30 +1,49 @@
 from extract import extract, pathway
 
 
-
-
+def remove_card_number(df_value):
+  
+        if df_value == None:
+            return df_value
+    
+        else:
+            split = df_value.split(",")
+    
+            return split[0]
+        
 
 def remove_sensitive_data(transaction_df):
     
     transaction_df.drop(columns="customer_name", inplace=True)
+        
+    transaction_df["card_type"] = transaction_df["card_type"].apply(lambda x: remove_card_number(x))       
+    
     print(transaction_df.head())
     
-    col = 'card_type'
-    for i in range(len(transaction_df[col])):
-        
-        current = transaction_df[col].iloc[i]
-        
-        if current == None:
-            continue
-       
-        else:
-            split = current.split(',')
-            transaction_df[col].iloc[i] = split[0]
-            
-    print(transaction_df.head())
     return transaction_df    
+
+
+def change_case(item_value):
+    
+    new_value = item_value.split(',')
+
+    store = []
+    
+    for item in new_value:
+        store.append(item.lower())
+    
+    return ','.join(store)
+
+
+def apply_change_case(transaction_df):
+    
+    transaction_df["basket"] = transaction_df["basket"].apply(lambda x: change_case(x))       
+    
+    print(transaction_df.head())
+    
+    return transaction_df  
 
 
 
 data = remove_sensitive_data(extract(pathway))
-
+data = apply_change_case(data)
