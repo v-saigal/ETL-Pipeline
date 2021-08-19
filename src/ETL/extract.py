e04a40168b61e628a5bdef1f1fb7794c282aefe4
Nyano <nayan100@hotmail.co.uk>
2021-08-19 19:54:42 +0100

    Made it into a class, now extracts either in a pandas or a normal dict format

--- src/ETL/extract.py
+++ src/ETL/extract.py
@@ -1,25 +1,27 @@
 import csv
-import json
-from os import remove
+
 import pandas as pd
 
 pathway = "../../data/2021-02-23-isle-of-wight.csv"
 fieldnames = ["timestamp","store_name","customer_name","basket", "payment_type", "total_price","card_type"]
 
+class Extract:
+    def __init__(self):
+        print('Starting to extract')
 
-def extract(pathway):
-    df = pd.read_csv(pathway, header=None)
-    df.rename(columns={0: "timestamp",
-                       1: "store_location",
-                       2: "customer_name",
-                       3: "basket",
-                       4:"payment_type",
-                       5:"total_price",
-                       6:"card_type"}, inplace=True)    
-    print(df.head())    
-    return df
-
-
-extract(pathway)
-
-
+    def extract_dict(self, pathway):
+        with open(pathway, mode='r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            
+            return list(reader)
+    
+    def extract_pandas(self):
+        df = pd.read_csv(pathway, header=None)
+        df.rename(columns={0: "timestamp",
+                        1: "store_location",
+                        2: "customer_name",
+                        3: "basket",
+                        4:"payment_type",
+                        5:"total_price",
+                        6:"card_type"}, inplace=True)
+        return df
