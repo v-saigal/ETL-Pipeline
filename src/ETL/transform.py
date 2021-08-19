8beb2610144744cfadee8a4aa82e5bb321f350c6
Nyano <nayan100@hotmail.co.uk>
2021-08-19 19:58:46 +0100

    Added function to clean the data using pandas, then tranform_transaction_format to convert the data in a format to make the basket table.

--- src/ETL/transform.py
+++ src/ETL/transform.py
@@ -1,49 +1,64 @@
-from extract import extract, pathway
+from helper_modules.helper_funcs import pretty_print_dict
+from cleaner import Cleaner
+from extract import Extract
 
+## Ignore this
+# dummy_transaction = [
+#     {'total': 22.0, 'item_basket': [
+#         {'size':'large', 'product_name': 'tea', 'price': 2.20},
+#         {'size': 'regular', 'product_name': 'coffee', 'price': 1.20}
+#         ]},
 
-def remove_card_number(df_value):
-  
-        if df_value == None:
-            return df_value
-    
-        else:
-            split = df_value.split(",")
-    
-            return split[0]
-        
+#     {'total': 44.0, 'item_basket': [
+#         {'size':'regular', 'product_name': 'chocolate tea', 'price': 2.3},
+#         {'size': 'regular', 'product_name': 'lemon tea', 'price': 1.40}
+#         ]},
+# ]
 
-def remove_sensitive_data(transaction_df):
+# pretty_print_dict(dummy_transaction)
+extract = Extract()
+
+# This needs to run just once, need to update
+def clean_data(extract):
+
+    cleaner  = Cleaner()
+    transaction_df = extract.extract_pandas()
     
     transaction_df.drop(columns="customer_name", inplace=True)
-        
-    transaction_df["card_type"] = transaction_df["card_type"].apply(lambda x: remove_card_number(x))       
     
-    print(transaction_df.head())
+    # Remove card number, change case, replace blanks
+    transaction_df["card_type"] = transaction_df["card_type"].apply(lambda x: cleaner.remove_card_number(x))
+    transaction_df["basket"] = transaction_df["basket"].apply(lambda x: cleaner.replace_blanks(x))     
+    transaction_df["basket"] = transaction_df["basket"].apply(lambda x: cleaner.change_case(x))   
     
-    return transaction_df    
+    transaction_df.to_csv('../../data/cleaned_data.csv', sep=',', index=False)
 
+clean_data(extract)
 
-def change_case(item_value):
+def transform_transaction_format(extract):
+    data = extract.extract_dict("../../data/cleaned_data.csv")
+    pretty_print_dict(data)
     
-    new_value = item_value.split(',')
-
     store = []
     
-    for item in new_value:
-        store.append(item.lower())
-    
-    return ','.join(store)
+    # This is used to make the basket table, each list contains each transaction,
+    # which then has the specific quanity for that particular item
 
-
-def apply_change_case(transaction_df):
-    
-    transaction_df["basket"] = transaction_df["basket"].apply(lambda x: change_case(x))       
-    
-    print(transaction_df.head())
-    
-    return transaction_df  
-
-
-
-data = remove_sensitive_data(extract(pathway))
-data = apply_change_case(data)
+    for each_transaction in data:
+        for keys in each_transaction:
+            if keys == "basket":
+                item_name = []
+                store_unique_item_quanity = {}
+                item_list = each_transaction[keys].split(',')
+                for i in list(range(0, len(item_list), 3)):
+                    item_name.append(item_list[i] + ' ' + item_list[i+1])
+                unique_item = set(item_name)
+                
+                for item in unique_item:
+                    store_unique_item_quanity[item] = item_name.count(item)
+                
+                store.append([store_unique_item_quanity])
+                pretty_print_dict(store)
+                
+                
+transform_transaction_format(extract)
