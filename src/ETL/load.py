1c7ae9a86c94b13cf8d1df94e24e4749105fc002
Nyano <nayan100@hotmail.co.uk>
2021-08-20 20:50:49 +0100

    Added code for setting up the size table

--- src/ETL/load.py
+++ src/ETL/load.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import psycopg2 as pg2
 
-from transform import transform_transaction_format
+from transform import transform_transaction_format, get_unique_item
 from helper_modules.helper_funcs import pretty_print_dict
 
 # Set up connection
@@ -41,4 +41,31 @@ def load_test():
     for data in basket:
         print(data)
 
-load_test()
+def load_size():
+    conn = db_connection_setup()
+    cur = conn.cursor()
+    sql = \
+        '''
+            INSERT INTO size (size_name)  
+            VALUES (%s)
+            
+        '''
+    
+    sizes = ['large', 'regular']
+    
+    for one_data in sizes:
+        try:
+            cur.execute(sql, (one_data,))
+        except:
+            print('cannot be added')
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def load_product():
+    basket = transform_transaction_format()
+    pretty_print_dict(basket)
+
+# load_product()
+load_size()
