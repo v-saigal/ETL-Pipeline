4bbfa4fe004d3230ee1a5be787bdc1efeb7bf3ee
Nyano <nayan100@hotmail.co.uk>
2021-08-29 08:50:47 +0100

    First changes of code to read through the files in the s3 bucket.

--- src/etl-group3/handler.py
+++ src/etl-group3/handler.py
@@ -1,10 +1,20 @@
 import json
 import boto3
+import os
+import csv
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v2.0! Your function executed successfully!",
-        "input": event,
+def lambda_handler(event, context):
+    # TODO implement
+    data = tt()
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data['Body'].read().decode('utf-8'))
     }
 
-    return {"statusCode": 200, "body": json.dumps(body)}
+def tt():
+    # year, month, day
+    key = '2021/8/25/birmingham_25-08-2021_09-00-00.csv'
+    client = boto3.client('s3')
+    
+    data = client.get_object(Bucket='delon3-team-3-bucket', Key=key)
+    return data
